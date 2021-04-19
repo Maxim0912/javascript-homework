6c1732b7571048abf561ce18b6d615d32aed0236
maxim <mver@list.ru>
2021-04-19 15:53:28 +0300

    homework-5 added another solution for Task#1

--- homework-5/js/task-5.js
+++ homework-5/js/task-5.js
@@ -1,12 +1,23 @@
 'use strict';
 
-//Task#1
+//Task#1 ver1
 
 function countIncludes(first, second) {
     let count = 0;
     let arrayFromString = first.split(" ");
     for (let string of arrayFromString) {
         if (string.includes(second)) count +=1;
+    }
+    return count;
+}
+
+//Task#1 ver2
+
+function countIncludes1(first, second) {
+    let count = 0;
+    let noSpaceString = first.toLowerCase().split(" ").join("");
+    for (let i = 0; i < noSpaceString.length; i += 1) {
+        if (noSpaceString.slice(i, i + second.length) === second) count +=1;
     }
     return count;
 }
